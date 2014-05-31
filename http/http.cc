2f64372ca8f54d585075d0f00084a38e75b7a66c
john doe <username@users.noreply.github.com>
2014-05-30 22:06:39 -0400

    misc changes

--- http/http.cc
+++ http/http.cc
@@ -7,6 +7,7 @@
 #include <my/jfile.h>
 #include <my/parse.h>
 #include <my/jsock.h>
+//#include <my/log.h>
 
 const char *msg=
     "HTTP/1.1 200 OK\n"
@@ -22,33 +23,51 @@ const char *msg=
     "</title>\n"
     "</head>\n"
     "<body>\n"
-    "<h2>Hey Now!</h2>\n"
+    "<h2>%s</h2>\n"
     "</body>\n"
     "</html>\n"
    ;
 
+int errmsg(jsock &sk, const char *s) {
+    char buf[4096];
+    int rc = snprintf(buf,sizeof(buf),msg,s);
+    return sk.write(buf,rc);
+}
+
 const char *root = "/www";
 
-/*
-extensions = [("gif", "image/gif" ),
-        ("jpg", "image/jpg" ),
-        ("jpeg","image/jpeg"),
-        ("png", "image/png" ),
-        ("ico", "image/ico" ),
-        ("zip", "image/zip" ),
-        ("gz",  "image/gz"  ),
-        ("tar", "image/tar" ),
-        ("htm", "text/html" ),
-        ("/", "text/html" ),  -- for index.html
-        ("html","text/html" ) ] :: [(String,String)]
-*/
+struct EXTS {
+    const char *fext, *mtype;
+};
+EXTS ext[] = {
+    {"gif", "image/gif" }
+    ,{"jpg", "image/jpg" }
+    ,{"jpeg","image/jpeg"}
+    ,{"png", "image/png" }
+    ,{"ico", "image/ico" }
+    ,{"zip", "image/zip" }
+    ,{"gz",  "image/gz"  }
+    ,{"tar", "image/tar" }
+    ,{"htm", "text/html" }
+    ,{"html","text/html" }
+    ,{0,0}
+};
+
+const char *getmimetype(const char *fn) {
+    int i, l, l2=strlen(fn);
+    for (i=0; ext[i].fext ;i++) {
+        l=strlen(ext[i].fext);
+        if (l2>=l && !strcasecmp(&fn[l2-l], ext[i].fext)) return ext[i].mtype;
+    }
+    return "";
+}
 
 int client(jsock &js) {
-char buf[4096];
+    char buf[4096];
     js.read(buf,sizeof(buf)-1);
     puts(buf);
     parse p(buf);
-    str s, iop, ifn, iver;
+    str s, iop, ifn, iver, ofn;
     int rc;
     rc=p.word(iop);  p.spaces();
     rc=p.word(ifn);  p.spaces();
@@ -61,14 +80,39 @@ char buf[4096];
         if (s.length()==0) break;
     }
 
-    if (iop == "GET" || iop=="POST") {
+    if (iop == "GET") { //|| iop=="POST") {
         str ofn;
         if (ifn=="/") ifn="/index.html";
         ofn=root + ifn;
         ofn.display();
-        jfile f(ofn.cstr()); // MOVE TO OUTER SCOPE and use open to catch errors
-        const char *buf=f.getFile();
-        if (buf) js.write(buf, strlen(buf));
+        // MOVE TO OUTER SCOPE and use open to catch errors
+
+        int acc;
+        if (access(ofn.cstr(),X_OK)==0) acc=2;
+        else if (access(ofn.cstr(),R_OK)==0) acc=1;
+        else {
+            errmsg(js,"Unable to access file");
+            return -1;
+        }
+        printf("acc=%d\n",acc);
+
+        if (acc==1) {
+            jfile f(ofn.cstr());
+            const char *buf2=f.getFile();
+            if (!buf2) {
+                errmsg(js,"getFile failed");
+                return -1;
+            }
+            snprintf(buf, sizeof(buf),
+                "HTTP/1.1 200 OK\nConnection: close\nContent-Type: %s\n\n",
+                getmimetype(ofn.cstr()));
+            js.write(buf, strlen(buf));
+            js.write(buf2, strlen(buf2));
+        }
+        else if (acc==2) {
+            errmsg(js,"CGI Not Implmented ");
+            return -1;
+        }
     }
     else js.write(msg, strlen(msg));
     //js.sync();
--- sys/jpd.cc
+++ sys/jpd.cc
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "jpopen.h"
 
 int main() {
     puts("hey");
     jpopen p;
     p.system("date");
+    char buf[256];
+    fgets(buf,sizeof(buf),stdin);
+    puts(buf);
+
+    int *fd = p.open("cat",3);
+    write(fd[1],buf,strlen(buf));
+    buf[0]=0;
+    fsync(fd[1]);
+    read(fd[0],buf,sizeof(buf));
+    puts(buf);
+    return 0;
 }
 
 
